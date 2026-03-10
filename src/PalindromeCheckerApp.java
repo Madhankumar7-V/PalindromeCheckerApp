4425d514df84c90530b530f0a9f99f7a38f26a69
Madhankumar7-V <mv4208@srmist.edu.in>
2026-03-10 10:45:46 +0530

    UC4: Palindome Check

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
 
-
 public class PalindromeCheckerApp {
+
+
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
@@ -9,15 +10,28 @@ public class PalindromeCheckerApp {
         System.out.print("Enter a string: ");
         String input = scanner.nextLine();
 
-        String reversed = "";
+
+        char[] characters = input.toCharArray();
+
+        int start = 0;
+        int end = characters.length - 1;
+
+        boolean isPalindrome = true;
 
 
-        for (int i = input.length() - 1; i >= 0; i--) {
-            reversed = reversed + input.charAt(i);
+        while (start < end) {
+
+            if (characters[start] != characters[end]) {
+                isPalindrome = false;
+                break;
+            }
+
+            start++;
+            end--;
         }
 
 
-        if (input.equals(reversed)) {
+        if (isPalindrome) {
             System.out.println("The string is a palindrome.");
         } else {
             System.out.println("The string is not a palindrome.");
